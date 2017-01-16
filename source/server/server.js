6750d334acd23c5552641fd6125081166b54dd72
nils <nils@harpoonandhatchet.com>
2017-01-16 03:45:44 -0500

    added basic server

---
+++ source/server/server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const exphbs = require('express-handlebars');
+const port = 2400;
+
+//for when static assets are used, if they are
+app.use(express.static('static'));
+
+app.engine('.hbs', exphbs({
+	extname: '.hbs',
+	defaultLayout: false
+}));
+
+app.set('view engine', '.hbs');
+
+app.get('/', function(req, res) {
+	res.render('index', { data: 'data', config: 'config' });
+});
+
+http.listen(port, function() {
+	console.log('listening on port %s', port);
+});
