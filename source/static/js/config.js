72b8704ddf95c2886bb6d73dc22ebdb8222d4e09
nils <nils@harpoonandhatchet.com>
2017-03-02 21:06:58 -0500

    added some params

--- source/static/js/config.js
+++ source/static/js/config.js
@@ -9,8 +9,19 @@ module.exports = {
 			spriteSheet: 'ships.json',
 			options: {
 				startPosition: { x: 200, y: 300 },
-				rotationConstraints: { pos: Infinity, neg: Infinity }
+				rotationConstraints: { pos: Infinity, neg: Infinity },
+				maxPositionVelocity: 4,
+				positionVelocityIncrement: .05,
+				postUpdates: [
+					function( delta ) {
+						// console.log( this.children[ 'rudder' ].currentRotation );
+						// console.log( this.rotationVelocity );
+						// this.children[ 'rudder' ].currentRotation = -this.currentRotation;
+						// this.children[ 'rudder' ].rotationVelocity = -this.rotationVelocity;
+					}
+				]
 			},
+
 			init: ( base ) => {
 				base.pivot.x = base.element.width / 2;
 				base.pivot.y = base.element.height;
@@ -121,7 +132,9 @@ module.exports = {
 					name: 'rudder',
 					id: 'turtle-rudder.png',
 					options: {
-						basePosition: { x: 46.23439168930054, y: 110 }
+						basePosition: { x: 46.23439168930054, y: 110 },
+						rotationConstraints: { pos: Util.toRadians( 20 ), neg: Util.toRadians( 20 ) },
+						stabilizeRotation: true
 					},
 					init: ( child , parent ) => {
 						child.pivot.x = child.element.width / 2;
