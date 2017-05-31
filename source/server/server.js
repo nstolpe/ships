a07d652f9b503ec9958cba7637b3813ca131a75a
nils <nils@harpoonandhatchet.com>
2017-05-30 21:54:50 -0400

    added 404 and dynamic single route

--- source/server/server.js
+++ source/server/server.js
@@ -16,17 +16,54 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs');
 
-app.get('/', function(req, res) {
-	res.render('index', { data: 'data', config: 'config' });
-});
+app.get('/*', function(req, res) {
+	res.render(
+		req.params[0] || 'index',
+		{
+			data: 'data', config: 'config'
+		},
+		function( err, html ) {
+			if ( err ) {
+				res.status( 404 ).send(
+					`<style>
+						* { margin: 0; }
+						.message {
+							position: relative;
+							top: 50%;
+							transform: translateY(-50%);
+							text-align: center;
+						}
+					</style>
+					<div id="message" class="message">
+						<h3>404</h3>
+						<p><em>/${ req.params[0] }</em> does not exist</p>
+					</div>
+					`
+				);
+			} else {
+				res.send(html);
+			}
+		}
+	);
+} );
 
-app.get('/gravity', function(req, res) {
-	res.render('gravity', { data: 'data', config: 'config' });
-});
+// app.get('/', function(req, res) {
+// 	console.log('slash');
+// 	res.render('index', { data: 'data', config: 'config' });
+// });
 
-app.get('/current', function(req, res) {
-	res.render('current', { data: 'data', config: 'config' });
-});
+// app.get('/gravity', function(req, res) {
+// 	res.render('gravity', { data: 'data', config: 'config' });
+// });
+
+// app.get('/current', function(req, res) {
+// 	console.log('current');
+// 	res.render('current', { data: 'data', config: 'config' });
+// });
+
+// app.get('/current', function(req, res) {
+// 	res.render('current', { data: 'data', config: 'config' });
+// });
 
 http.listen(port, function() {
 	console.log('listening on port %s', port);
