132d2f0fe9979d55a81d07954f5329bf9c5472cb
nils <slincire@gmail.com>
2017-05-31 00:06:12 -0400

    updated comment

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Setup stuff here, like directories. {..} syntax doesn't work in package.json
+# Setup stuff here, like directories. Because NPM uses sh, the brace expansion doesn't work in package.json
 mkdir -p source/server/.static/assets/{spritesheets,images,js}
