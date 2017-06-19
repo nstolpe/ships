05f46a6c13e5061030721581925759b56e01867c
nils <nils@harpoonandhatchet.com>
2017-06-18 20:18:56 -0400

    refactored watch to look for require instead of using the include tags

--- bin/watch.js
+++ bin/watch.js
@@ -38,11 +38,11 @@ chokidar.watch( jsPath + 'inc/*.js', {
 		ignored: /(^|[\/\\])\../
 	} )
 	.on( 'change', function( path ) {
-		console.log( 'here' );
 		const segments = path.split( '/' );
 		const fileName = segments[ segments.length - 1 ];
 		const incRx = /{INCLUDES}([\s\S]*){\/INCLUDES}/g;
 		const jsRx = /([\w\-. ]+\.js)/g;
+		const reqRx = /require\(\s*?'([\w\-. \/]*\.js)'\s*?\)/g;
 
 		// get the contents of the `jsPath` dir that are scripts by filtering for `.js` extension.
 		// ignores directories, like `inc`
@@ -53,35 +53,25 @@ chokidar.watch( jsPath + 'inc/*.js', {
 			// contents of one of the javascript files under `jsPath`
 			let fileText = fs.readFileSync( jsPath + scripts[ i ], 'utf8' );
 
-			// match each block of INCLUDES, there should really be only one
-			while ( includeMatch = incRx.exec( fileText ) ) {
-				let includeText = includeMatch[ 1 ];
+			// match each `require( '...' )` statement
+			while ( reqMatch = reqRx.exec( fileText ) ) {
+				if ( fileName === reqMatch[ 1 ].substring( reqMatch[ 1 ].length - fileName.length ) ) {
+					let proc = spawn( 'browserify', [ jsPath + scripts[ i ], '-o', jsBuildPath + scripts[ i ] ], {
+						stdio: 'inherit'
+					} );
 
-				// match each included js file from `inc/`
-				while ( jsMatch = jsRx.exec( includeText ) ) {
+					console.log( `recompiling \`${ jsBuildPath + scripts[ i ] }\` due to changes to \`${ path }\`` );
 
-					// if `fileName` is one of the includes, compile
-					if ( jsMatch[ 1 ] === fileName ) {
-						console.log(jsPath + scripts[ i ]);
-						console.log(jsBuildPath + scripts[ i ]);
-						let proc = spawn( 'browserify', [ jsPath + scripts[ i ], '-o', jsBuildPath + scripts[ i ] ], {
-							stdio: 'inherit'
-						} );
+					proc.on( 'close', code => {
+						if ( code === 1) 
+							console.error( `✖ "browserify ${ path } -o ${ jsBuildPath + fileName }" failed`);
+						else
+							console.log('Watching srcipts...');
+					} );
 
-
-						console.log( `recompiling \`${ jsBuildPath + scripts[ i ] }\` due to changes to \`${ path }\`` );
-
-						proc.on( 'close', code => {
-							if ( code === 1) 
-								console.error( `✖ "browserify ${ path } -o ${ jsBuildPath + fileName }" failed`);
-							else
-								console.log('Watching srcipts...');
-						} );
-
-						// no need to keep looking after one has been matched, recompiling the top level will 
-						// pull in changes of included files
-						break;
-					}
+					// no need to keep looking after one has been matched, recompiling the top level will 
+					// pull in changes of included files
+					break;
 				}
 			}
 		}
