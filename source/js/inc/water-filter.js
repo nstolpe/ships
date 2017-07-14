29ebc93d01eae4c98141041217e9ef40b7575610
nils <nils@harpoonandhatchet.com>
2017-07-14 01:55:27 -0400

    made it darken things a little

--- source/js/inc/water-filter.js
+++ source/js/inc/water-filter.js
@@ -27,16 +27,18 @@ const ShaderSource = {
 			'uniform vec4 filterArea;',
 			'float blurRed( vec2 pos ) {',
 			'	float color = 0.0;',
-			'	for (int i = -9; i <= 9; i++) {',
-			'		for (int j = -9; j <= 9; j++) {',
+			'	for (int i = -1; i <= 1; i++) {',
+			'		for (int j = -1; j <= 1; j++) {',
 			'			color += texture2D( uSampler, pos + vec2( i, j ) / filterArea.xy ).r;',
 			'		}',
 			'	}',
-			'	return color / 361.0;',
+			'	return color / 9.0;',
 			'}',
 			'void main( void ) {',
 			'	gl_FragColor = texture2D( uSampler, vTextureCoord );',
-			// '	gl_FragColor.r = blurRed( vTextureCoord );',
+			'	gl_FragColor.r = max( 0.0, gl_FragColor.r * 0.8);',
+			'	gl_FragColor.g = max( 0.0, gl_FragColor.g * 0.8);',
+			'	gl_FragColor.b = max( 0.0, gl_FragColor.b * 0.8);',
 			'}'
 		].join( '\n' );
 	}
