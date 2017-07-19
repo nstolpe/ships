e41cc31c14c2f8f1532016aacd27c34ed4124376
nils <slincire@gmail.com>
2017-07-18 22:32:30 -0400

    commented some sub-sprites

--- source/js/inc/config.js
+++ source/js/inc/config.js
@@ -52,105 +52,105 @@ module.exports = function( PIXI, app ) {
 				init: ( base ) => {
 					base.pivot.x = base.sprite.width / 2;
 					base.pivot.y = base.sprite.height / 2;
-					base.children.rudder.currentPosition.x = base.sprite.width / 2;
-					base.children.rudder.basePosition.x = base.sprite.width / 2;
+					// base.children.rudder.currentPosition.x = base.sprite.width / 2;
+					// base.children.rudder.basePosition.x = base.sprite.width / 2;
 				},
 				children: [
 					// left cannons
-					{
-						name: 'cannon-left-mid',
-						id: 'turtle-cannon-large.png',
-						options: {
-							basePosition: { x: 0, y: 60 },
-							baseRotation: Util.toRadians( 180 ),
-							rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.04,
-							rotationVelocityIncrement: 0.004,
-						},
-						init: ( child , parent ) => {
-							child.pivot.y = child.sprite.height / 2;
-							child.basePosition.x = child.sprite.width;
-							child.currentPosition.x = child.sprite.width;
-						}
-					},
-					{
-						name: 'cannon-left-bow',
-						id: 'turtle-cannon-small.png',
-						options: {
-							basePosition: { x: 41, y: 27 },
-							baseRotation: Util.toRadians( -157 ),
-							rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.04,
-							rotationVelocityIncrement: 0.004
-						},
-						init: ( child , parent ) => {
-							child.pivot.y = child.sprite.height / 2;
-						} 
-					},
-					{
-						name: 'cannon-left-aft',
-						id: 'turtle-cannon-small.png',
-						options: {
-							basePosition: { x: 41, y: 93 },
-							baseRotation: Util.toRadians( 157 ),
-							rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.04,
-							rotationVelocityIncrement: 0.004
-						},
-						init: ( child , parent ) => {
-							child.pivot.y = child.sprite.height / 2;
-						} 
-					},
-					// right cannons
-					{
-						name: 'cannon-right-mid',
-						id: 'turtle-cannon-large.png',
-						options: {
-							basePosition: { x: 79, y: 60 },
-							rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.04,
-							rotationVelocityIncrement: 0.004
-						},
-						init: ( child , parent ) => {
-							child.pivot.y = child.sprite.height / 2;
-							// child.basePosition.x = child.sprite.width;
-							// child.currentPosition.x = child.sprite.width;
-						} 
-					},
-					{
-						name: 'cannon-right-bow',
-						id: 'turtle-cannon-small.png',
-						options: {
-							basePosition: { x: 75, y: 27 },
-							baseRotation: Util.toRadians( -23 ),
-							rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.04,
-							rotationVelocityIncrement: 0.004
-						},
-						init: ( child , parent ) => {
-							child.pivot.y = child.sprite.height / 2;
-						} 
-					},
-					{
-						name: 'cannon-right-aft',
-						id: 'turtle-cannon-small.png',
-						options: {
-							basePosition: { x: 75, y: 93 },
-							baseRotation: Util.toRadians( 23 ),
-							rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.04,
-							rotationVelocityIncrement: 0.004
-						},
-						init: ( child , parent ) => {
-							child.pivot.y = child.sprite.height / 2;
-						} 
-					},
+					// {
+					// 	name: 'cannon-left-mid',
+					// 	id: 'turtle-cannon-large.png',
+					// 	options: {
+					// 		basePosition: { x: 0, y: 60 },
+					// 		baseRotation: Util.toRadians( 180 ),
+					// 		rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.04,
+					// 		rotationVelocityIncrement: 0.004,
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.y = child.sprite.height / 2;
+					// 		child.basePosition.x = child.sprite.width;
+					// 		child.currentPosition.x = child.sprite.width;
+					// 	}
+					// },
+					// {
+					// 	name: 'cannon-left-bow',
+					// 	id: 'turtle-cannon-small.png',
+					// 	options: {
+					// 		basePosition: { x: 41, y: 27 },
+					// 		baseRotation: Util.toRadians( -157 ),
+					// 		rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.04,
+					// 		rotationVelocityIncrement: 0.004
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.y = child.sprite.height / 2;
+					// 	} 
+					// },
+					// {
+					// 	name: 'cannon-left-aft',
+					// 	id: 'turtle-cannon-small.png',
+					// 	options: {
+					// 		basePosition: { x: 41, y: 93 },
+					// 		baseRotation: Util.toRadians( 157 ),
+					// 		rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.04,
+					// 		rotationVelocityIncrement: 0.004
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.y = child.sprite.height / 2;
+					// 	} 
+					// },
+					// // right cannons
+					// {
+					// 	name: 'cannon-right-mid',
+					// 	id: 'turtle-cannon-large.png',
+					// 	options: {
+					// 		basePosition: { x: 79, y: 60 },
+					// 		rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.04,
+					// 		rotationVelocityIncrement: 0.004
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.y = child.sprite.height / 2;
+					// 		// child.basePosition.x = child.sprite.width;
+					// 		// child.currentPosition.x = child.sprite.width;
+					// 	} 
+					// },
+					// {
+					// 	name: 'cannon-right-bow',
+					// 	id: 'turtle-cannon-small.png',
+					// 	options: {
+					// 		basePosition: { x: 75, y: 27 },
+					// 		baseRotation: Util.toRadians( -23 ),
+					// 		rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.04,
+					// 		rotationVelocityIncrement: 0.004
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.y = child.sprite.height / 2;
+					// 	} 
+					// },
+					// {
+					// 	name: 'cannon-right-aft',
+					// 	id: 'turtle-cannon-small.png',
+					// 	options: {
+					// 		basePosition: { x: 75, y: 93 },
+					// 		baseRotation: Util.toRadians( 23 ),
+					// 		rotationConstraints: { pos: Util.toRadians( 16 ), neg: Util.toRadians( 16 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.04,
+					// 		rotationVelocityIncrement: 0.004
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.y = child.sprite.height / 2;
+					// 	} 
+					// },
 					{
 						name: 'body',
 						id: 'turtle-body.png',
@@ -163,25 +163,25 @@ module.exports = function( PIXI, app ) {
 							// child.pivot.x = parent.width / 2;
 						}
 					},
-					{
-						name: 'rudder',
-						id: 'turtle-rudder.png',
-						options: {
-							basePosition: { x: 46.23439168930054, y: 110 },
-							// rotationConstraints: { pos: Util.toRadians( 20 ), neg: Util.toRadians( 20 ) },
-							positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
-							maxRotationVelocity: 0.02,
-							rotationVelocityIncrement: 0.01,
-							// stabilizeRotation: true,
-							debug: true
-						},
-						init: ( child , parent ) => {
-							child.pivot.x = child.sprite.width / 2;
+					// {
+					// 	name: 'rudder',
+					// 	id: 'turtle-rudder.png',
+					// 	options: {
+					// 		basePosition: { x: 46.23439168930054, y: 110 },
+					// 		// rotationConstraints: { pos: Util.toRadians( 20 ), neg: Util.toRadians( 20 ) },
+					// 		positionConstraints: { pos: { x: 0, y: 0 }, neg: { x: 0, y: 0 } },
+					// 		maxRotationVelocity: 0.02,
+					// 		rotationVelocityIncrement: 0.01,
+					// 		// stabilizeRotation: true,
+					// 		debug: true
+					// 	},
+					// 	init: ( child , parent ) => {
+					// 		child.pivot.x = child.sprite.width / 2;
 
-							child.currentPosition.x = parent.sprite.width / 2;
-							child.basePosition.x = child.currentPosition.x;
-						}
-					}
+					// 		child.currentPosition.x = parent.sprite.width / 2;
+					// 		child.basePosition.x = child.currentPosition.x;
+					// 	}
+					// }
 				]
 			}
 		]
