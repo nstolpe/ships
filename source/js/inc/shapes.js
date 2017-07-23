a175b829f694cb0e120a9bc82ff717ef0e684fdc
nils <nils@harpoonandhatchet.com>
2017-07-23 19:07:55 -0400

    moved some base functionality to shape, factories to use it

--- source/js/inc/shapes.js
+++ source/js/inc/shapes.js
@@ -1,18 +1,46 @@
 'use strict';
 const Vec = require( 'victor' );
 
+function Shape( options ) {
+	return Object.create( {
+		position: Vec.prototype.isPrototypeOf( options.position ) ? options.position : Array.isArray( options.position ) ? Vec.fromArray( options.position ) : Vec( 0, 0 ),
+		color: options.color || 0x000000,
+		mass: isNaN( options.mass ) ? 0 : options.mass,
+		velocity: Vec( 0, 0 ),
+		forces: options.forces || [],
+		preUpdates: Array.isArray( options.preUpdates ) ? options.preUpdates : [],
+		postUpdates: Array.isArray( options.postUpdates ) ? options.postUpdates : [],
+		preUpdate( delta ) {
+			for ( let i = 0, l = this.preUpdates.length; i < l; i++ )
+				this.preUpdates[ i ].call( this, delta );
+		},
+		postUpdate( delta ) {
+			for ( let i = 0, l = this.postUpdates.length; i < l; i++ )
+				this.postUpdates[ i ].call( this, delta );
+		},
+		/**
+		 * Updates the properties of the Circle from options. @TODO dynamically go through options.
+		 */
+		update( options, delta ) {
+			this.preUpdate( delta );
+			this.color = options.color || this.color;
+			this.position = Vec.prototype.isPrototypeOf( options.position ) ? options.position : Array.isArray( options.position ) ? Vec.fromArray( options.position ) : this.position,
+			this.radius = isNaN( options.radius ) ? this.radius : options.radius;
+			this.mass = isNaN( options.mass ) ? this.mass : options.mass;
+			this.velocity = options.velocity || this.velocity;
+			this.postUpdate( delta );
+		}
+	} );
+};
+
 module.exports = {
-	Circle( props ) {
-		return {
-			color: props.color || 0x000000,
-			position: Vec.prototype.isPrototypeOf( props.position ) ? props.position : Array.isArray( props.position ) ? Vec.fromArray( props.position ) : Vec( 0, 0 ),
-			radius: isNaN( props.radius ) ? 50 : props.radius,
-			mass: isNaN( props.mass ) ? 0 : props.mass,
-			velocity: Vec( 0, 0 ),
-			preUpdates: Array.isArray( props.preUpdates ) ? props.preUpdates : [],
-			postUpdates: Array.isArray( props.postUpdates ) ? props.postUpdates : [],
-			forces: props.forces || [],
+	Circle( options ) {
+		const base = Shape( options );
+
+		return Object.assign( base, {
+			radius: isNaN( options.radius ) ? 50 : options.radius,
 			drawMethod: 'drawCircle',
+			static: false,
 			drawArgs() {
 				return [
 					this.position.x,
@@ -20,26 +48,22 @@ module.exports = {
 					this.radius
 				];
 			},
+			updateBase: base.update,
 			/**
-			 * Updates the properties of the Circle from props. @TODO dynamically go through props.
+			 * Updates the properties of the Circle from options. @TODO dynamically go through options.
 			 */
-			update( props, delta ) {
+			update( options, delta ) {
 				this.preUpdate( delta );
-				this.color = props.color || this.color;
-				this.position = Vec.prototype.isPrototypeOf( props.position ) ? props.position : Array.isArray( props.position ) ? Vec.fromArray( props.position ) : this.position,
-				this.radius = isNaN( props.radius ) ? this.radius : props.radius;
-				this.mass = isNaN( props.mass ) ? this.mass : props.mass;
-				this.velocity = props.velocity || this.velocity;
+				this.updateBase( options, delta );
 				this.postUpdate( delta );
-			},
-			preUpdate( delta ) {
-				for ( let i = 0, l = this.preUpdates.length; i < l; i++ )
-					this.preUpdates[ i ].call( this, delta );
-			},
-			postUpdate( delta ) {
-				for ( let i = 0, l = this.postUpdates.length; i < l; i++ )
-					this.postUpdates[ i ].call( this, delta );
 			}
-		}
+		}, base );
+	},
+	Rectangle( options ) {
+		const base = Shape( options );
+
+		return Object.assign( base, {
+			dimensions: 0
+		} );
 	}
-}
+};
