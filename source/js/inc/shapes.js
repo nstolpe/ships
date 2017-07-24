571e7d6862d2dc77ad5a3904f758a6fd8de11412
nils <nils@harpoonandhatchet.com>
2017-07-23 23:29:30 -0400

    refactored proto chain

--- source/js/inc/shapes.js
+++ source/js/inc/shapes.js
@@ -2,62 +2,140 @@
 const Vec = require( 'victor' );
 
 function Shape( options ) {
-	return Object.create( {
-		position: Vec.prototype.isPrototypeOf( options.position ) ? options.position : Array.isArray( options.position ) ? Vec.fromArray( options.position ) : Vec( 0, 0 ),
-		color: options.color || 0x000000,
-		mass: isNaN( options.mass ) ? 0 : options.mass,
-		velocity: Vec( 0, 0 ),
-		forces: options.forces || [],
-		preUpdates: Array.isArray( options.preUpdates ) ? options.preUpdates : [],
-		postUpdates: Array.isArray( options.postUpdates ) ? options.postUpdates : [],
-		preUpdate( delta ) {
-			for ( let i = 0, l = this.preUpdates.length; i < l; i++ )
-				this.preUpdates[ i ].call( this, delta );
+	const propsData = {
+		color: 0x000000,
+		mass: 0,
+		forces: [],
+		preUpdates: [],
+		postUpdates: [],
+		position: Vec()
+	};
+
+	const props = {
+		position:{
+			enumerable: true,
+			get: () => propsData.position,
+			set: ( position ) => {
+				propsData.position = Vec.prototype.isPrototypeOf( position ) ? position :
+									Array.isArray( position ) ? Vec.fromArray( position ) :
+									typeof position === 'object' ? Vec.fromObject( position ) :
+									Vec( 0, 0 )
+			}
 		},
-		postUpdate( delta ) {
-			for ( let i = 0, l = this.postUpdates.length; i < l; i++ )
-				this.postUpdates[ i ].call( this, delta );
+		color: {
+			enumerable: true,
+			get: () => propsData.color,
+			set: ( color ) => propsData.color = color === 'null' || isNaN( color ) ? propsData.color : parseFloat( color )
 		},
-		/**
-		 * Updates the properties of the Circle from options. @TODO dynamically go through options.
-		 */
-		update( options, delta ) {
-			this.preUpdate( delta );
-			this.color = options.color || this.color;
-			this.position = Vec.prototype.isPrototypeOf( options.position ) ? options.position : Array.isArray( options.position ) ? Vec.fromArray( options.position ) : this.position,
-			this.radius = isNaN( options.radius ) ? this.radius : options.radius;
-			this.mass = isNaN( options.mass ) ? this.mass : options.mass;
-			this.velocity = options.velocity || this.velocity;
-			this.postUpdate( delta );
+		mass: {
+			enumerable: true,
+			get: () => propsData.mass,
+			set: ( mass ) => propsData.mass = isNaN( mass ) ? propsData.mass : parseFloat( mass )
+		},
+		velocity: {
+			value: Vec( 0, 0 ),
+			enumerable: true,
+			writable: true
+		},
+		forces: {
+			enumerable: true,
+			get: () => propsData.forces,
+			// propsData.forces is always the same array. Errors in update. Good? Bad? Weird? Warning for this set?
+			set: ( forces ) => Array.isArray( forces ) ? Array.prototype.push.apply( propsData.forces, forces ) : false
+		},
+		preUpdates: {
+			enumerable: true,
+			get: () => propsData.preUpdates,
+			set: ( preUpdates ) => Array.isArray( preUpdates ) ? Array.prototype.push.apply( propsData.preUpdates, preUpdates ) : false
+		},
+		postUpdates: {
+			enumerable: true,
+			get: () => propsData.postUpdates,
+			set: ( postUpdates ) => Array.isArray( postUpdates ) ? Array.prototype.push.apply( propsData.postUpdates, postUpdates ) : false
+		},
+		static: {
+			enumerable: true,
+			value: false,
+			writable: true
+		},
+		preUpdate: {
+			value: function( delta ) {
+				for ( let i = 0, l = this.preUpdates.length; i < l; i++ )
+					this.preUpdates[ i ].call( this, delta );
+			}
+		},
+		postUpdate: {
+			value: function( delta ) {
+				for ( let i = 0, l = this.postUpdates.length; i < l; i++ )
+					this.postUpdates[ i ].call( this, delta );
+			}
+		},
+		update: {
+			value: function( options, delta ) {
+				this.preUpdate( delta );
+				this.color = options.color || this.color;
+				this.position = options.position,
+				this.radius = isNaN( options.radius ) ? this.radius : options.radius;
+				this.mass = isNaN( options.mass ) ? this.mass : options.mass;
+				this.velocity = options.velocity || this.velocity;
+				this.postUpdate( delta );
+			},
+			writable: true
 		}
-	} );
+	};
+
+	const shape = Object.create( {}, props );
+
+	shape.color = options.color;
+	shape.mass = options.mass;
+	shape.forces = options.forces;
+	shape.preUpdates = options.preUpdates;
+	shape.postUpdates = options.postUpdates;
+	shape.position = options.position;
+
+	return shape;
 };
 
 module.exports = {
 	Circle( options ) {
 		const base = Shape( options );
 
-		return Object.assign( base, {
-			radius: isNaN( options.radius ) ? 50 : options.radius,
-			drawMethod: 'drawCircle',
-			static: false,
-			drawArgs() {
-				return [
-					this.position.x,
-					this.position.y,
-					this.radius
-				];
+		const propsData = {
+			radius: 50,
+		};
+
+		const props = {
+			radius: {
+				enumerable: true,
+				get: () => propsData.radius,
+				set: ( radius ) => propsData.radius = typeof radius === 'null' || isNaN( radius ) ? propsData.radius : parseFloat( radius )
 			},
-			updateBase: base.update,
-			/**
-			 * Updates the properties of the Circle from options. @TODO dynamically go through options.
-			 */
-			update( options, delta ) {
-				this.preUpdate( delta );
-				this.updateBase( options, delta );
-				this.postUpdate( delta );
+			drawMethod: {
+				value: 'drawCircle'
+			},
+			drawArgs: {
+				value: function() {
+					return [
+						this.position.x,
+						this.position.y,
+						this.radius
+					];
+				}
+			},
+			update: {
+				value: function( options, delta ) {
+					this.preUpdate( delta );
+					Object.getPrototypeOf( this ).update( options, delta );
+					this.postUpdate( delta );
+				}
 			}
-		}, base );
+		};
+
+		const circle = Object.create( base, props );
+
+		circle.radius = options.radius;
+
+		return circle;
 	},
 	Rectangle( options ) {
 		const base = Shape( options );
