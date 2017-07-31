52db447161d9a4d316da6c1d3a966c955f8ca607
nils <slincire@gmail.com>
2017-07-30 21:04:12 -0400

    moved code to script. added pixi-particles

---
+++ bin/compile-scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for js in source/js/*.js; do
+	echo "compiling $js";
+	browserify "$js" -o .static/assets/js/$(basename "$js") --noparse=$PWD/node_modules/pixi-particles/dist/pixi-particles.min.js;
+done
