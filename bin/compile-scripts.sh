34390cbb9921f5c6078b47d4f54fce8f8fb7e518
nils <slincire@gmail.com>
2017-07-30 22:42:49 -0400

    use absolute pwd with browserify

--- bin/compile-scripts.sh
+++ bin/compile-scripts.sh
@@ -2,5 +2,5 @@
 
 for js in source/js/*.js; do
 	echo "compiling $js";
-	browserify "$js" -o .static/assets/js/$(basename "$js") --noparse=$PWD/node_modules/pixi-particles/dist/pixi-particles.min.js;
+	browserify "$js" -o .static/assets/js/$(basename "$js") --noparse=$(pwd -P)/node_modules/pixi-particles/dist/pixi-particles.min.js;
 done
