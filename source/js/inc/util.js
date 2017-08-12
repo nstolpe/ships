6ea4ce12c2682222c57c5dd7f455f606beafac17
nils <nils@harpoonandhatchet.com>
2017-08-12 19:28:19 -0400

    added isNumeric

--- source/js/inc/util.js
+++ source/js/inc/util.js
@@ -1,12 +1,6 @@
 'use strict'
 
 module.exports = {
-	toDegrees( angle ) {
-		return angle * ( 180 / Math.PI );
-	},
-	toRadians( angle ) {
-		return angle * ( Math.PI / 180 );
-	},
 	/**
 	 * Enum-like immutable object with 3 states.
 	 */
@@ -14,5 +8,18 @@ module.exports = {
 		POSITIVE: 1,
 		NEUTRAL: 0,
 		NEGATIVE: -1
-	} )
-}
+	} ),
+	toDegrees( angle ) {
+		return angle * ( 180 / Math.PI );
+	},
+	toRadians( angle ) {
+		return angle * ( Math.PI / 180 );
+	},
+	/**
+	 * Checks is a value is numeric
+	 * https://stackoverflow.com/questions/9716468/is-there-any-function-like-isnumeric-in-javascript-to-validate-numbers#answer-16655847
+	 */
+	isNumeric( val ) {
+		return Number( parseFloat( val ) ) == val;
+	}
+};
