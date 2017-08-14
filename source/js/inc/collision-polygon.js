ad060fb73a6a65d23b44b06c32165020ef9490ab
nils <nils@harpoonandhatchet.com>
2017-08-14 00:50:35 -0400

    converted edges and normals to vector2

--- source/js/inc/collision-polygon.js
+++ source/js/inc/collision-polygon.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const PIXI = require( 'pixi.js' );
+const Vec2 = require( './vector2.js' );
 
 function Vector( x, y ) {
 	const proto = {
@@ -19,17 +20,17 @@ module.exports = function ( ...points ) {
 		setEdges() {
 			// create an edge and normal between each set of points.
 			for ( let i = 0, l = this.points.length; i < l; i += 2 ) {
-				let p1 = { x: poly.points[ i ], y: poly.points[ i + 1 ] };
+				let p1 = Vec2( poly.points[ i ], poly.points[ i + 1 ] );
 				// need to start again and grab the original point's x and y
-				let p2 = i + 2 < l ? { x: poly.points[ i + 2 ], y: poly.points[ i + 3 ] } : { x: poly.points[ 0 ], y: poly.points[ 1 ] };
-				let edge = { x: p2.x - p1.x, y: p2.y - p1.y };
-				let perp = { x: edge.y, y: -edge.x };
-				let perpLength = Math.sqrt( perp.x * perp.x + perp.y * perp.y );
-				let normal = perpLength != 0 ? { x: perp.x / perpLength, y: perp.y / perpLength } : perp;
-				this.edges[ i ] = edge.x;
-				this.edges[ i + 1 ] = edge.y;
-				this.normals[ i ] = normal.x;
-				this.normals[ i + 1 ] = normal.y;
+				let p2 = i + 2 < l ? Vec2( poly.points[ i + 2 ], poly.points[ i + 3 ] ) : Vec2( poly.points[ 0 ], poly.points[ 1 ] );
+				let edge = Vec2( p2.x - p1.x, p2.y - p1.y );
+				let perp = edge.copy().perp();
+				let perpLength = perp.len();
+				let normal = perp.copy().nor();
+				this.edges[ i ] = edge;
+				this.normals[ i ] = normal;
+				// this.edges[ i ] = edge.x;
+				// this.edges[ i + 1 ] = edge.y;
 			}
 		}
 	};
