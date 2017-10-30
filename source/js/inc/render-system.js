36f24ca501e222f441f6c1f8573929d8930e8b1f
nils <nils@harpoonandhatchet.com>
2017-10-30 01:52:03 -0400

    refactored

--- source/js/inc/render-system.js
+++ source/js/inc/render-system.js
@@ -5,16 +5,7 @@ const Components = ECS.Components;
 const System = ECS.System;
 
 const RenderSystem = function( options ) {
-    const app = new PIXI.Application(
-        options.view.clientWidth * options.resolution,
-        options.view.clientHeight * options.resolution,
-        {
-            view: options.view,
-            backgroundColor: options.backgroundColor,
-            resolution: options.resolution,
-            autoresize: true
-        }
-    );
+    const App = options.App;
 
     const system = Object.create( System, {
         'start': {
@@ -23,37 +14,44 @@ const RenderSystem = function( options ) {
                 Object.getPrototypeOf( this ).start();
 
                 const entities = this.getEntities();
-                // @TODO this window listener needs to move.
-                window.addEventListener( 'resize', function() {
-                    app.renderer.resize(
-                        app.view.clientWidth * app.renderer.resolution,
-                        app.view.clientHeight * app.renderer.resolution
-                    );
-                }, false );
+
                 // get visual and transform data and create a child for the `PIXI.application` stage
                 // @TODO only handles Sprites. needs at support TilingSprite and other possibilities.
                 // `multi` visuals will work as they'll be individual components
                 entities.forEach( entity => {
+                    // @TODO better entity api
                     const spriteComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Sprite ) ||
                         entity.components.find( component => Object.getPrototypeOf( component ) === Components.TilingSprite );
-                    const positionComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Position );
-                    const rotationComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Rotation );
-                    const scaleComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Scale );
-
-                    spriteComponent.data.position.x = positionComponent.data.x;
-                    spriteComponent.data.position.y = positionComponent.data.y;
-                    spriteComponent.data.rotation = rotationComponent.data;
-                    spriteComponent.data.scale.x = scaleComponent.data;
-                    spriteComponent.data.scale.y = scaleComponent.data;
-                    spriteComponent.data.pivot.set( spriteComponent.data.width * 0.5, spriteComponent.data.height * 0.5 );
-                    app.stage.addChild( spriteComponent.data );
+                    this.updateEntity( entity );
+                    App.stage.addChild( spriteComponent.data );
                 } );
+                App.renderer.backgroundColor = options.backgroundColor;
+                this.setListeners();
+                App.ticker.add( this.update.bind( this ) );
             }
         },
         'update': {
             value: function() {
+                // console.log('update');
                 const entities = this.getEntities();
-                console.log( entities );
+                entities.forEach( entity => this.updateEntity( entity ) );
+            }
+        },
+        'updateEntity': {
+            value: function( entity ) {
+                // @TODO add querying functions to components so these don't need to be so long and messy.
+                const spriteComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Sprite ) ||
+                    entity.components.find( component => Object.getPrototypeOf( component ) === Components.TilingSprite );
+                const positionComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Position );
+                const rotationComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Rotation );
+                const scaleComponent = entity.components.find( component => Object.getPrototypeOf( component ) === Components.Scale );
+
+                spriteComponent.data.position.x = positionComponent.data.x;
+                spriteComponent.data.position.y = positionComponent.data.y;
+                spriteComponent.data.rotation = rotationComponent.data;
+                spriteComponent.data.scale.x = scaleComponent.data;
+                spriteComponent.data.scale.y = scaleComponent.data;
+                spriteComponent.data.pivot.set( spriteComponent.data.width * 0.5, spriteComponent.data.height * 0.5 );
             }
         },
         'getEntities': {
@@ -67,6 +65,16 @@ const RenderSystem = function( options ) {
                 } );
 
                 return entities;
+            }
+        },
+        'setListeners': {
+            value: function() {
+                window.addEventListener( 'resize', function() {
+                    App.renderer.resize(
+                        App.view.clientWidth * App.renderer.resolution,
+                        App.view.clientHeight * App.renderer.resolution
+                    );
+                }, false );
             }
         }
     } );
