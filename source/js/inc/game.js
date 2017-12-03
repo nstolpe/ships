f2fb2c0d2f1570fb3666244e97e25125f73cb5d4
nils <nils@harpoonandhatchet.com>
2017-12-03 01:12:38 -0500

    added graphics setup and touch analog joystick stuff

--- source/js/inc/game.js
+++ source/js/inc/game.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const PIXI = require( 'pixi.js' );
-const Matter = require( 'matter-js' );
+// @TODO fix w/ browserify shim. https://github.com/liabru/matter-js/issues/365
 const decomp = require('poly-decomp');
 window.decomp = decomp;
-
+const Matter = require( 'matter-js' );
+const VJS = require( 'virtualjoystick.js' );
+window.VJS = VJS;
 const Turms = require( 'turms' );
 const Util = require( './util.js' );
 const ECS = require( './ecs.js' );
@@ -136,6 +138,9 @@ module.exports = function( id, view, scale ) {
             const resources = this.loader.resources;
             let skinningComponent;
             let texture;
+            let fillComponent;
+            let strokeComponent;
+            let radiusComponent;
 
             switch ( type ) {
                 case 'sprite':
@@ -144,8 +149,8 @@ module.exports = function( id, view, scale ) {
                     break;
                 case 'tiling-sprite':
                     texture = this.getTexture( actor, resources );
-                    const w = Util.property(actor.geometry, 'width' );
-                    const h = Util.property(actor.geometry, 'height' );
+                    const w = Util.property( actor.geometry, 'width' );
+                    const h = Util.property( actor.geometry, 'height' );
                     skinningComponent = Components.TilingSprite.create( texture, w, h );
                     break;
                 case 'compound':
@@ -159,6 +164,15 @@ module.exports = function( id, view, scale ) {
                         childEntity.addComponents( renderComponent );
                         skinningComponent.data.addChild( renderComponent.data );
                     } );
+                    break;
+                case 'graphics':
+                    fillComponent = Components.Fill.create( Util.property( actor.geometry, 'display.fill' ) )
+                    strokeComponent = Components.Stroke.create(
+                        Util.property( actor.geometry, 'display.stroke' ),
+                        Util.property( actor.geometry, 'display.stroke-width' )
+                    );
+                    entity.addComponents( fillComponent, strokeComponent );
+                    skinningComponent = Components.Graphics.create();
                     break;
                 default:
                     break;
@@ -331,96 +345,164 @@ module.exports = function( id, view, scale ) {
     }
 }
 
-// @TODO move this out
-document.getElementById( 'view' ).addEventListener( 'keydown', e => {
-    let type;
-    let data;
+// @TODO move this out. parameterize selectors
+function activateInputs() {
+    const touchJoystick = Object.create( VJS.prototype );
+    VJS.call( touchJoystick, {
+        container: document.getElementById( 'view-wrapper' ),
+        limitStickTravel: true,
+        stickRadius: 20,
+        baseRadius: 40,
+        travelRadius: 30,
+        strokeStyle: 'rgba(0, 255, 255, 0.25)'
+    } );
+    touchJoystick.addEventListener( 'touchStart', function( e ) {
+        console.log( e );
+    } );
+    touchJoystick.addEventListener( 'touchMove', function( e ) {
+        const up = this.up();
+        const down = this.down();
+        const left = this.left();
+        const right = this.right();
+        const angle = Math.atan2(  this._stickY - this._baseY, this._stickX - this._baseX ) * 180 / Math.PI;
+        const xDiff = this._stickX - this._baseX;
+        const yDiff = this._stickY - this._baseY;
+        const distance = Math.sqrt( xDiff * xDiff + yDiff * yDiff );
+        console.log( 'x: ' + this.deltaX() );
+        console.log( 'y: ' + this.deltaY() );
+        console.log( `%cangle: ${ angle }`, 'color: white; background-color: black;' );
+        console.log( `%cdistance: ${ distance }`, 'color: cyan; background-color: red;' );
+        // if ( up || down )
+        //     hub.sendMessage( { type: 'player-input-thrust', data: up ? 1 * .1: -1 * .1} );
+        // else
+        //     hub.sendMessage( { type: 'player-input-thrust', data: 0 } );
 
-    switch ( e.which ) {
-        // W
-        case 87:
-            if ( !e.repeat ) {
+        // if ( left || right )
+        //     hub.sendMessage( { type: 'player-input-turn', data: right ? 1 * .2 : -1 * .2 } );
+        // else
+        //     hub.sendMessage( { type: 'player-input-turn', data: 0 } );
+
+        if ( this.deltaY() )
+            hub.sendMessage( { type: 'player-input-thrust', data: 1 * .1 * -this.deltaY() / this._travelRadius } );
+        if ( this.deltaX() )
+            hub.sendMessage( { type: 'player-input-turn', data: 1 * .2 * this.deltaX() / this._travelRadius } );
+
+//     -90
+//      |
+// 180--|--0
+//      |
+//      90
+    } );
+    touchJoystick.addEventListener( 'touchEnd', function( e ) {
+        hub.sendMessage( { type: 'player-input-thrust', data: 0 } );
+        hub.sendMessage( { type: 'player-input-turn', data: 0 } );
+    } );
+    document.getElementById( 'view' ).addEventListener( 'keydown', e => {
+        let type;
+        let data;
+
+        switch ( e.which ) {
+            // W
+            case 87:
+                if ( !e.repeat ) {
+                    type = 'player-input-thrust';
+                    data = 1 * .1;
+                }
+                break;
+            // S
+            case 83:
+                if ( !e.repeat ) {
+                    type = 'player-input-thrust';
+                    data = -1 * .1;
+                }
+                break;
+            // A
+            case 65:
+                if ( !e.repeat ) {
+                    type = 'player-input-turn'
+                    data = -1 * .2;
+                }
+                break;
+            // D
+            case 68:
+                if ( !e.repeat ) {
+                    type = 'player-input-turn'
+                    data = 1 * .2;
+                }
+                break;
+            // P
+            case 80:
+                if ( !e.repeat ) {
+                    type = 'player-input-boost'
+                    data = 20;
+                }
+                break;
+            // <
+            case 188:
+                if ( e.shiftKey && !e.repeat ) {
+                    type = 'player-input-rotate-viewport'
+                    data = -1;
+                }
+                break;
+            // >
+            case 190:
+                if ( e.shiftKey && !e.repeat ) {
+                    type = 'player-input-rotate-viewport'
+                    data = 1;
+                }
+                break;
+            default:
+                break;
+        }
+
+        if ( type !== undefined && data !== undefined )
+            hub.sendMessage( { type: type, data: data } );
+    }, false );
+
+    document.getElementById( 'view' ).addEventListener( 'keyup', e => {
+        let type;
+        let data;
+
+        switch ( e.which ) {
+            // W
+            case 87:
                 type = 'player-input-thrust';
-                data = 1 * .1;
-            }
-            break;
-        // S
-        case 83:
-            if ( !e.repeat ) {
+                data = 0;
+                break;
+            // S
+            case 83:
                 type = 'player-input-thrust';
-                data = -1 * .1;
-            }
-            break;
-        // A
-        case 65:
-            if ( !e.repeat ) {
-                type = 'player-input-turn'
-                data = -1 * .2;
-            }
-            break;
-        // D
-        case 68:
-            if ( !e.repeat ) {
-                type = 'player-input-turn'
-                data = 1 * .2;
-            }
-            break;
-        // P
-        case 80:
-            if ( !e.repeat ) {
-                type = 'player-input-boost'
-                data = 20;
-            }
-            break;
-        default:
-            break;
-    }
+                data = 0;
+                break;
+            // A
+            case 65:
+                type = 'player-input-turn';
+                data = 0;
+                break;
+            // D
+            case 68:
+                type = 'player-input-turn';
+                data = 0;
+                break;
+            // X
+            case 88:
+                type = 'player-input-dock';
+                data = 0;
+                break;
+            default:
+                break;
+        }
 
-    if ( type !== undefined && data !== undefined )
-        hub.sendMessage( { type: type, data: data } );
-}, false );
+        if ( type !== undefined && data !== undefined )
+            hub.sendMessage( { type: type, data: data } );
+    }, false );
 
-document.getElementById( 'view' ).addEventListener( 'keyup', e => {
-    let type;
-    let data;
+    document.getElementById( 'view' ).addEventListener( 'wheel', e => {
+        // the zoom delta needs to be inverted and scaled.
+        console.log( e );
+        if ( e.deltaY !== 0 )
+            hub.sendMessage( { type: 'zoom', data: e.deltaY * -0.001 } );
+    }, false );
+}
 
-    switch ( e.which ) {
-        // W
-        case 87:
-            type = 'player-input-thrust';
-            data = 0;
-            break;
-        // S
-        case 83:
-            type = 'player-input-thrust';
-            data = 0;
-            break;
-        // A
-        case 65:
-            type = 'player-input-turn';
-            data = 0;
-            break;
-        // D
-        case 68:
-            type = 'player-input-turn';
-            data = 0;
-            break;
-        // X
-        case 88:
-            type = 'player-input-dock';
-            data = 0;
-            break;
-        default:
-            break;
-    }
-
-    if ( type !== undefined && data !== undefined )
-        hub.sendMessage( { type: type, data: data } );
-}, false );
-
-document.getElementById( 'view' ).addEventListener( 'wheel', e => {
-    // the zoom delta needs to be inverted and scaled.
-    console.log( e );
-    if ( e.deltaY !== 0 )
-        hub.sendMessage( { type: 'zoom', data: e.deltaY * -0.001 } );
-}, false );
+activateInputs();
