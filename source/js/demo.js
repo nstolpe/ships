23fe4f0da66d33b9f4d5ca0b330ce23799e8ef5d
nils <nils@harpoonandhatchet.com>
2018-01-19 21:43:54 -0500

    spaces. new attributes

--- source/js/demo.js
+++ source/js/demo.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const Turms = require( 'turms' );
-const Util = require( './inc/util.js' );
-const Game = require( './inc/game.js' );
+const Turms = require('turms');
+const Util = require('./inc/util.js');
+const Game = require('./inc/game.js');
 
-const env = Object.assign( {
+const env = Object.assign({
     id: 'default'
-}, Util.locationArgs( window ) );
+}, Util.locationArgs(window));
 
-const game = Game(
-    env.id,
-    document.getElementById( 'view' ),
-    window.devicePixelRatio
-).load();
+const game = Game({
+    id: env.id,
+    viewAttributes: {
+        id: 'view',
+        className: 'view',
+    },
+    wrapperSelector: '#view-wrapper',
+    resolution: window.devicePixelRatio
+}).load();
