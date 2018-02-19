0525f945495d25945ffa23c9c7727fb5f2ba3381
nils <nils@harpoonandhatchet.com>
2018-02-19 02:59:01 -0500

    added globify. switched it in over dependency watcher

--- bin/watch.js
+++ bin/watch.js
@@ -15,96 +15,98 @@ const dataBuildPath = '.static/assets/data/';
 const imageBuildPath = '.static/assets/images/';
 const spritesheetBuildPath = '.static/assets/spritesheets/';
 
+// @DEPRECATED
+// @TODO remove this
 /**
  * watches the js files in source for changes and recompiles with browserify when they're made.
  */
-chokidar.watch( jsPath + '*.js', {
-		ignored: /(^|[\/\\])\../
-	} )
-	.on( 'change', function( filePath ) {
-		const segments = filePath.split( '/' );
-		const fileName = segments[ segments.length - 1 ];
+// chokidar.watch( jsPath + '*.js', {
+// 		ignored: /(^|[\/\\])\../
+// 	} )
+// 	.on( 'change', function( filePath ) {
+// 		const segments = filePath.split( '/' );
+// 		const fileName = segments[ segments.length - 1 ];
 
-		console.log( `recompiling \`${ jsBuildPath + fileName }\` due to changes to \`${ filePath }\`` );
-		const proc = spawn(
-			'browserify',
-			[
-				filePath,
-				'-o',
-				jsBuildPath + fileName,
-				`--noparse=${ path.join( __dirname, '..' ) }/node_modules/pixi-particles/dist/pixi-particles.min.js`
-			],
-			{
-				stdio: 'inherit'
-			}
-		);
-		proc.on( 'close', code => {
-			if ( code === 1)
-				console.error( `✖ "browserify ${ filePath } -o ${ jsBuildPath + fileName }" failed`);
-			else
-				console.log('watching scripts...');
-		} );
-	} );
+// 		console.log( `recompiling \`${ jsBuildPath + fileName }\` due to changes to \`${ filePath }\`` );
+// 		const proc = spawn(
+// 			'browserify',
+// 			[
+// 				filePath,
+// 				'-o',
+// 				jsBuildPath + fileName,
+// 				`--noparse=${ path.join( __dirname, '..' ) }/node_modules/pixi-particles/dist/pixi-particles.min.js`
+// 			],
+// 			{
+// 				stdio: 'inherit'
+// 			}
+// 		);
+// 		proc.on( 'close', code => {
+// 			if ( code === 1)
+// 				console.error( `✖ "browserify ${ filePath } -o ${ jsBuildPath + fileName }" failed`);
+// 			else
+// 				console.log('watching scripts...');
+// 		} );
+// 	} );
 /**
  * Watches all files in the include directory (default: `source/js/inc`) for changes.
  * On change, parses through all files in `jsPath` directory (default: `source/js`), matches
  * calls to `require()`, looks for the file name inside them, and recompiles the files under `jsPath`.
  */
 // chokidar.watch( jsPath + 'inc/*.js', {
-chokidar.watch( jsPath + 'inc/', {
-		ignored: /(^|[\/\\])\../
-	} )
-	.on( 'change', function( filePath ) {
-		const segments = filePath.split( '/' );
-		const fileName = segments[ segments.length - 1 ];
-		const jsRx = /([\w\-. ]+\.js)/g;
-		// matches require statements
-		const reqRx = /require\(\s*?'([\w\-. \/]*\.js)'\s*?\)/g;
+// chokidar.watch( jsPath + 'inc/', {
+// 		ignored: /(^|[\/\\])\../
+// 	} )
+// 	.on( 'change', function( filePath ) {
+// 		const segments = filePath.split( '/' );
+// 		const fileName = segments[ segments.length - 1 ];
+// 		const jsRx = /([\w\-. ]+\.js)/g;
+// 		// matches require statements
+// 		const reqRx = /require\(\s*?'([\w\-. \/]*\.js)'\s*?\)/g;
 
-		// get the contents of the `jsPath` dir that are scripts by filtering for `.js` extension.
-		// ignores directories, like `inc`
-		const scripts = fs.readdirSync( jsPath ).filter( ( s ) => /\.js$/i.test( s ) );
+// 		// get the contents of the `jsPath` dir that are scripts by filtering for `.js` extension.
+// 		// ignores directories, like `inc`
+// 		const scripts = fs.readdirSync( jsPath ).filter( ( s ) => /\.js$/i.test( s ) );
 
-		// loop through the javascript files
-		for ( let i = 0, l = scripts.length; i < l; i++ ) {
-			// contents of one of the javascript files under `jsPath`
-			let fileText = fs.readFileSync( jsPath + scripts[ i ], 'utf8' );
+// 		// loop through the javascript files
+// 		for ( let i = 0, l = scripts.length; i < l; i++ ) {
+// 			// contents of one of the javascript files under `jsPath`
+// 			let fileText = fs.readFileSync( jsPath + scripts[ i ], 'utf8' );
 
-			// match each `require( '...' )` statement
-			// but first this, otherwise the exec doesn't get all matches? @TODO Fix. Text matches in Node CLI and Chrome browser console.
-			fileText.match( reqRx );
-			while ( reqMatch = reqRx.exec( fileText ) ) {
-				// console.log(reqMatch[ 1 ].substring( reqMatch[ 1 ].length - fileName.length ));
-				if ( fileName === reqMatch[ 1 ].substring( reqMatch[ 1 ].length - fileName.length ) ) {
-					let proc = spawn(
-						'browserify',
-						[
-							jsPath + scripts[ i ],
-							'-o',
-							jsBuildPath + scripts[ i ],
-							`--noparse=${ path.join( __dirname, '..' ) }/node_modules/pixi-particles/dist/pixi-particles.min.js`
-						],
-						{
-							stdio: 'inherit'
-						}
-					);
+// 			// match each `require( '...' )` statement
+// 			// but first this, otherwise the exec doesn't get all matches? @TODO Fix. Text matches in Node CLI and Chrome browser console.
+// 			fileText.match( reqRx );
+// 			while ( reqMatch = reqRx.exec( fileText ) ) {
+// 				// console.log(reqMatch[ 1 ].substring( reqMatch[ 1 ].length - fileName.length ));
+// 				if ( fileName === reqMatch[ 1 ].substring( reqMatch[ 1 ].length - fileName.length ) ) {
+// 					let proc = spawn(
+// 						'browserify',
+// 						[
+// 							jsPath + scripts[ i ],
+// 							'-o',
+// 							jsBuildPath + scripts[ i ],
+// 							`--noparse=${ path.join( __dirname, '..' ) }/node_modules/pixi-particles/dist/pixi-particles.min.js`
+// 						],
+// 						{
+// 							stdio: 'inherit'
+// 						}
+// 					);
 
-					console.log( `recompiling \`${ jsBuildPath + scripts[ i ] }\` due to changes to \`${ filePath }\`` );
+// 					console.log( `recompiling \`${ jsBuildPath + scripts[ i ] }\` due to changes to \`${ filePath }\`` );
 
-					proc.on( 'close', code => {
-						if ( code === 1 )
-							console.error( `✖ "browserify ${ filePath } -o ${ jsBuildPath + fileName }" failed` );
-						else
-							console.log( 'Watching scripts...' );
-					} );
+// 					proc.on( 'close', code => {
+// 						if ( code === 1 )
+// 							console.error( `✖ "browserify ${ filePath } -o ${ jsBuildPath + fileName }" failed` );
+// 						else
+// 							console.log( 'Watching scripts...' );
+// 					} );
 
-					// no need to keep looking after one has been matched, recompiling the top level will
-					// pull in changes of included files
-					break;
-				}
-			}
-		}
-	} );
+// 					// no need to keep looking after one has been matched, recompiling the top level will
+// 					// pull in changes of included files
+// 					break;
+// 				}
+// 			}
+// 		}
+// 	} );
 
 /**
  * watches source/images for changes and copies them to imageBuildPath
